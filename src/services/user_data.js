de8d1e7162b6aa9af7255dbf9f6ca2303e9afabc
AnnaM317 <anya317@gmail.com>
2021-11-30 22:05:15 +0200

    data_anna

---
+++ src/services/user_data.js
@@ -0,0 +1,35 @@
+"user" = [
+    {
+        "_id": utilService.makeId(),
+        "fullname": "Marlon Bordo",
+        "imgUrls": "https://res.cloudinary.com/disku3v4j/image/upload/v1638300062/our_kind_of_people_lance_gross_2x_ssvg9h.jpg",
+        "isAdmin": false,
+        "isHost": true,
+        "username": "marlo",
+        "password": "123"
+    },
+    {
+        "_id": utilService.makeId(),
+        "fullname": "Bonnie Bonno",
+        "imgUrls": "https://res.cloudinary.com/disku3v4j/image/upload/v1638299789/host_qszv7p.jpg",
+        "isAdmin": false,
+        "isHost": true,
+        "username": "bonnie",
+        "password": "123"
+    },
+    {
+        "_id": utilService.makeId(),
+        "fullname": "Tee Tyrel",
+        "imgUrls": "https://res.cloudinary.com/disku3v4j/image/upload/v1638300901/pexels-photo-1300402_wtxf88.jpg",
+        "isAdmin": false,
+        "isHost": true,
+        "username": "tee",
+        "password": "123"
+    },
+
+
+
+
+
+
+]
