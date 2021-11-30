96cec5a448dd38d8a8862f5dbbd25163e7227eaa
Angelina-K <79867604+Angelina-K@users.noreply.github.com>
2021-11-30 22:51:29 +0200

    added stay store

---
+++ src/store/stay.store.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import { stayService } from '../services/stay.service.js';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  strict: true,
+  modules: {},
+  state: {},
+  mutations: {},
+  actions: {},
+});
--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,20 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-import { userStore } from './user.store.js'
-import { reviewStore } from './review.store.js'
+import { userStore } from './user.store.js';
+import { reviewStore } from './review.store.js';
+import { stayStore } from './stay.store.js';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   strict: true,
   modules: {
     userStore,
-    reviewStore
+    reviewStore,
+    stayStore,
   },
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  }
-})
+  state: {},
+  mutations: {},
+  actions: {},
+});
