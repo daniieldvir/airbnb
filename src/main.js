546dfa5e2608c09e2b08aea71bd08fb69cc3ebdb
AnnaM317 <anya317@gmail.com>
2021-12-01 23:09:55 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -6,6 +6,20 @@ import store from './store';
 import locale from 'element-ui/lib/locale/lang/en';
 import ElementUI from 'element-ui';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+
+// library.add(faFontAwesome)
+
+
 import './assets/styles/main.scss';
 
 Vue.config.productionTip = false;
