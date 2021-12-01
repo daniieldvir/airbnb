2bbb08eecd007c2c70ab0527841ac6b5304d39b7
Angelina-K <79867604+Angelina-K@users.noreply.github.com>
2021-12-01 10:17:06 +0200

    loading stays

--- src/store/stay.store.js
+++ src/store/stay.store.js
@@ -19,24 +19,23 @@ export const stayStore = {
       state.filterBy = filterBy;
     },
     setStays(state, { stays }) {
-      console.log('stays mutation', stays);
       state.stays = stays;
+      console.log('stays', state.stays);
     },
   },
   actions: {
-    loadStays(context, { filterBy }) {
-      const stays = stayService
-        .query()
-        .then(context.commit({ type: 'setStays', stays }));
-
-      //   try {
-      //     const stays = await stayService.query();
-      //     context.commit({ type: 'setStay', stays });
-      //   } catch (err) {
-      //     console.log('stayService: Error in loading stays', err);
-      //     throw err;
-      //   }
+    loadStays({ commit }) {
+      stayService.query().then((stays) => commit({ type: 'setStays', stays }));
     },
+    // async loadStays(context, { filterBy }) {
+    //     try {
+    //       const stays = await stayService.query();
+    //       context.commit({ type: 'setStay', stays });
+    //     } catch (err) {
+    //       console.log('stayService: Error in loading stays', err);
+    //       throw err;
+    //     }
+    // },
   },
   modules: {},
 };
