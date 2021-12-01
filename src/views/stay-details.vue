1312969f17f772dfdcd1a3bded22809c0d02727d
daniieldvir <91826695+daniieldvir@users.noreply.github.com>
2021-12-01 12:27:23 +0200

    routes

--- src/views/stay-details.vue
+++ src/views/stay-details.vue
@@ -1,43 +1,36 @@
 <template>
-<section class="header">
-			<div class="details">
-					<h2>{{ stay.name }}</h2>
-			</div>
+  <section class="header">
+    <div class="details">
+      <h2>{{ stay.name }}</h2>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-	props: {},
-	components: {
-	},
-	data() {
-		return {
-			isLoading: true,
-      stay: null
-		}
-	},
+  props: {},
+  components: {},
+  data() {
+    return {
+      isLoading: true,
+      stay: null,
+    };
+  },
   created: {
-    loadStay(){
-      const { stayId } = this.$route.params
-      this.$store.dispatch({ type: 'getStayById', stayId })
-      .then((stay) => {
+    loadStay() {
+      const { stayId } = this.$route.params;
+      this.$store.dispatch({ type: 'getStayById', stayId }).then((stay) => {
         console.log(stay);
-        this.stay = JSON.parse(JSON.stringify(stay))
-      })
+        this.stay = JSON.parse(JSON.stringify(stay));
+      });
       // this.$store.dispatch.loadStay();
-    }
+    },
   },
-	computed: {
-
-		stay() {
-			return this.$store.getters.currStay
-		},
-
-	},
-	methods: {
-
+  computed: {
+    stay() {
+      return this.$store.getters.currStay;
+    },
   },
-  
-}
-
+  methods: {},
+};
 </script>
