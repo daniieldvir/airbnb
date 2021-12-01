11980992e42143a8b54ea158159d9983adaa1ebe
daniieldvir <91826695+daniieldvir@users.noreply.github.com>
2021-12-01 15:00:18 +0200

    perview

--- src/cmps/stay-preview.vue
+++ src/cmps/stay-preview.vue
@@ -2,7 +2,8 @@
   <section v-if="stay" class="stay-preview">
     <router-link class="router-link" :to="'/stay/' + stay._id">
       <img class="preview_img" :src="stay.imgUrls[0]" alt="" />
-      <p>{{ stay.name }}</p>
+      <p>{{ stay.type }}</p>
+      <p>{{ stay.name }} · {{ stay.loc.country }}</p>
       <p>
         <span>${{ stay.price }}</span> / night
       </p>
