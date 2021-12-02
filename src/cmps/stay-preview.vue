52a23d71ffe249c3da63b56a315b3bd281d53ee5
daniieldvir <91826695+daniieldvir@users.noreply.github.com>
2021-12-02 11:46:14 +0200

    map + list + pont

--- src/cmps/stay-preview.vue
+++ src/cmps/stay-preview.vue
@@ -1,7 +1,12 @@
 <template>
   <section v-if="stay" class="stay-preview">
     <div class="block">
-      <el-carousel trigger="click" arrow="always" height="200px" :autoplay="false">
+      <el-carousel
+        trigger="click"
+        arrow="always"
+        height="200px"
+        :autoplay="false"
+      >
         <el-carousel-item v-for="(img, idx) in stay.imgUrls" :key="idx">
           <router-link class="router-link" :to="'/stay/' + stay._id">
             <img class="preview_img" :src="stay.imgUrls[idx]" alt="" />
@@ -11,8 +16,8 @@
     </div>
 
     <router-link class="router-link" :to="'/stay/' + stay._id">
-      <p>{{ stay.type }}</p>
-      <p>{{ stay.name }} · {{ stay.loc.country }}</p>
+      <p class="stay-type">{{ stay.type }}</p>
+      <p>{{ sortTxt }} · {{ stay.loc.country }}</p>
 
       <p>
         <span>${{ stay.price }}</span> / night
@@ -22,14 +27,26 @@
 </template>
 
 <script>
+import priceRangeFilter from './price-range-filter.vue';
+import priceRange from './price-range-filter.vue';
 export default {
+  components: { priceRangeFilter },
   name: 'stay-preview',
   props: ['stay'],
   data() {
     return {};
   },
   computed: {
-    rate() {},
+    rate() {
+      const rate = foundRate.map();
+    },
+    sortTxt() {
+      var txt = this.stay.name;
+      if (txt.length > 15) return txt.slice(0, 15) + '...';
+    },
+  },
+  components: {
+    priceRange,
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -5,29 +5,38 @@ import router from './router';
 import store from './store';
 import locale from 'element-ui/lib/locale/lang/en';
 import ElementUI from 'element-ui';
+import * as VueGoogleMaps from 'vue2-google-maps';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(fas)
+library.add(fas);
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 // import { library } from '@fortawesome/fontawesome-svg-core'
 // import { faFontAwesome } from '@fortawesome/free-brands-svg-icons'
 
 // library.add(faFontAwesome)
 
-
 import './assets/styles/main.scss';
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI, { locale });
-
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyAiwks8bz483lqJ9B_71_Q0LqoN6LKT5LE',
+    libraries: 'places',
+    load: {
+      region: 'IL',
+      language: 'En',
+    },
+  },
+}),
+  new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+  }).$mount('#app');
