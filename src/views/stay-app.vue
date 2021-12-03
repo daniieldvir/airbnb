3188da0ba2defcf5224d6720a7b276351244fe79
Angelina-K <79867604+Angelina-K@users.noreply.github.com>
2021-12-03 13:43:35 +0200

    ...

--- src/views/stay-app.vue
+++ src/views/stay-app.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="stay-app main-container">
     <app-header />
+    <secondary-filters />
     <stay-list :stays="stays"> </stay-list>
   </section>
 </template>
@@ -8,9 +9,10 @@
 <script>
 import stayList from '../cmps/stay-list.vue';
 import appHeader from '../cmps/app-header.vue';
+import secondaryFilters from '../cmps/filters/secondary-filters.vue';
 
 export default {
-  components: { stayList, appHeader },
+  components: { stayList, appHeader, secondaryFilters },
   name: 'stay-app',
   computed: {
     stays() {
