b9dd7243cd96821467bca12d88cc76b5e879f5a6
Angelina-K <79867604+Angelina-K@users.noreply.github.com>
2021-12-04 13:15:15 +0200

    home page cards

---
+++ src/cmps/lists/home-list.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <div class="list-container column main-container">
+      <h2>{{ listTitle }}</h2>
+      <ul class="home-list clean-list">
+        <li
+          class="card pointer"
+          v-for="place in places"
+          :key="place.imgUrls[0]"
+          @click="cardClicked(place)"
+        >
+          <img class="card-img" :src="place.imgUrls[0]" alt="" />
+          <h4>{{ place.name }}</h4>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'home-list',
+  props: ['places', 'listTitle'],
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {
+    cardClicked(place) {
+      this.$emit('cardClicked', place);
+    },
+  },
+  components: {},
+};
+</script>
+<style></style>
